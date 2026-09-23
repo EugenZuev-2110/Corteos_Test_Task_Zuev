c707751a9cdac3c1479b2eb287f8b0a9a423f690
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-09-23 03:09:15 +0300

    ICbrClient created

---
+++ Corteos_Test_Task_Zuev/Domain/Interfaces/ICbrClient.cs
@@ -0,0 +1,11 @@
+﻿using Corteos_Test_Task_Zuev.Domain.Entities;
+
+namespace Corteos_Test_Task_Zuev.Domain.Interfaces;
+
+public interface ICbrClient
+{
+    /// <summary>
+    /// Получает курсы валют от ЦБ РФ за конкретную дату.
+    /// </summary>
+    Task<IEnumerable<CurrencyRate>> GetRatesByDateAsync(DateTime date, CancellationToken cancellationToken = default);
+}
