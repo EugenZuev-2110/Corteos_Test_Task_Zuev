aea5f4fa623ced3349275ee48d02242fa20bfd49
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-09-23 03:12:22 +0300

    ICurrencyRateRepository created

---
+++ Corteos_Test_Task_Zuev/Domain/Interfaces/ICurrencyRateRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Corteos_Test_Task_Zuev.Domain.Interfaces;
+
+public interface ICurrencyRateRepository
+{
+    /// <summary>
+    /// Проверяет наличие записей за конкретную дату.
+    /// </summary>
+    Task<bool> HasDataForDateAsync(DateTime date, CancellationToken cancellationToken = default);
+}
