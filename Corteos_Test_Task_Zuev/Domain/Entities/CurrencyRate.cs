f53153a11853e0b6889ec307ea1e1fdb36e74cda
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-09-23 03:03:56 +0300

    CurrencyRate created

---
+++ Corteos_Test_Task_Zuev/Domain/Entities/CurrencyRate.cs
@@ -0,0 +1,42 @@
+﻿namespace Corteos_Test_Task_Zuev.Domain.Entities;
+
+/// <summary>
+/// Сущность курса валюты, оптимизированная под 3НФ.
+/// </summary>
+public class CurrencyRate
+{
+    /// <summary>
+    /// Внутренний код валюты ЦБ РФ (например, "R01235" для USD).
+    /// </summary>
+    public string CurrencyId { get; private set; } = null!;
+
+    /// <summary>
+    /// Буквенный код валюты (ISO) (например, "USD").
+    /// </summary>
+    public string CharCode { get; private set; } = null!;
+
+    /// <summary>
+    /// Цифровой код валюты (например, "840").
+    /// </summary>
+    public string NumCode { get; private set; } = null!;
+
+    /// <summary>
+    /// Полное наименование валюты (например, "Доллар США").
+    /// </summary>
+    public string Name { get; private set; } = null!;
+
+    /// <summary>
+    /// Дата, к которой относится курс.
+    /// </summary>
+    public DateTime Date { get; private set; }
+
+    /// <summary>
+    /// Номинал (количество единиц валюты, для которых указан курс, например, 1 или 100).
+    /// </summary>
+    public int Nominal { get; private set; }
+
+    /// <summary>
+    /// Значение курса к рублю.
+    /// </summary>
+    public decimal Value { get; private set; }
+}
