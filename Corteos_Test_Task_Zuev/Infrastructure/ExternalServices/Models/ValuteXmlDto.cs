414ba835aae7a27154367f17d3e0e4401c4830af
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-09-23 03:36:21 +0300

    ValuteXmlDto created

---
+++ Corteos_Test_Task_Zuev/Infrastructure/ExternalServices/Models/ValuteXmlDto.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Serialization;
+
+namespace Corteos_Test_Task_Zuev.Infrastructure.ExternalServices.Models;
+
+public class ValuteXmlDto
+{
+    [XmlAttribute("ID")]
+    public string Id { get; set; } = null!;
+
+    [XmlElement("NumCode")]
+    public string NumCode { get; set; } = null!;
+
+    [XmlElement("CharCode")]
+    public string CharCode { get; set; } = null!;
+
+    [XmlElement("Nominal")]
+    public int Nominal { get; set; }
+
+    [XmlElement("Name")]
+    public string Name { get; set; } = null!;
+
+    [XmlElement("Value")]
+    public string ValueString { get; set; } = null!;
+}
